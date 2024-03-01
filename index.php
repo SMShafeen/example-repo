fee88f7dfade054091e95686d56ba33b5cfe63f0
SMShafeen <shafeenpc123@gmail.com>
2024-03-01 16:21:46 +0530

    index php
    hi

--- index.php
+++ index.php
@@ -1,12 +1,56 @@
+<?php
+
+require_once "config.php";
+
+if(isset($_POST['submit'])){
+  $name = mysqli_real_escape_string($conn, $_POST['first_name']);
+  $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
+  $email = mysqli_real_escape_string($conn, $_POST['email']);
+  // $password = mysqli_real_escape_string($conn, password_hash($password, PASSWORD_DEFAULT));
+  $password = mysqli_real_escape_string($conn, md5($_POST['password']));
+  $cpassword = mysqli_real_escape_string($conn, md5($_POST['confirm_password']));
+
+  $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$password'") or die('Query Failed!');
+    if(mysqli_num_rows($select_users) > 0){
+        $message[] = 'User already exist!';
+    }
+    else{
+       if($password != $cpassword){
+        $message[] = 'Confirm password not matched!';
+       }
+       else{
+        mysqli_query($conn, "INSERT INTO `users`(first_name, last_name, email, password) VALUES ('$name', '$last_name', '$email', '$password')") or die('Query Failed!');
+        $message[] = 'Registered Successfully!';
+        header('location: index.php');
+       }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css" />
-    <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
   </head>
+
+  <?php
+  
+  if(isset($message)){
+    foreach($message as $message){
+        echo '<div class="message">
+                    <span>'.$message.'</span>
+                    <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+              </div>';
+    }
+  }
+
+  ?>
+
   <body class="bg-sky-200 flex items-center justify-center">
     <div class="w-[55%] mt-24 bg-slate-50 flex rounded-lg overflow-hidden">
       <div class="left w-[50%] bg-blue-500">
@@ -36,7 +80,7 @@
             REGISTER FORM
           </h2>
         </div>
-        <form action="process_registration.php" method="post">
+        <form action="" method="post">
           <div class="form grid grid-cols-2 m-2 ml-6">
             <div class="first_name flex flex-col p-2">
               <label for="first_name" class="text-[1.1rem]">First Name</label>
@@ -91,12 +135,13 @@
               /><br />
             </div>
           </div>
+          <input
+          name="submit"
+            type="submit"
+            value="Register"
+            class="cursor-pointer bg-blue-500 text-cyan-50 p-3 pl-10 pr-10 ml-36 mt-4"
+          />
         </form>
-        <input
-          type="submit"
-          value="Register"
-          class="cursor-pointer bg-blue-500 text-cyan-50 p-3 pl-10 pr-10 ml-36 mt-4"
-        />
       </div>
     </div>
   </body>
